a0e12f7cf74b1b9b246d58a05f1c6142e9da380b
Hung Nguyen Viet <hvn@familug.org>
2016-05-08 15:20:32 +0700

    rename API class

--- anphode/main.py
+++ anphode/main.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 
-from resources.restaurant import RestaurantAPI
+from resources.restaurant import Restaurant
 import db
 
 
@@ -13,14 +13,13 @@ def create_app():
 
 app = create_app()
 api_ = Api(app)
-api_.add_resource(RestaurantAPI, '/restaurant/')
+api_.add_resource(Restaurant, '/restaurant/')
 
 
 @app.route("/")
-def hello():
+def listing():
     return "Hello World!"
 
 
 if __name__ == "__main__":
-    db.db.create_all()
     app.run()
--- anphode/resources/restaurant.py
+++ anphode/resources/restaurant.py
@@ -2,9 +2,9 @@ from flask_restful import Resource
 import db
 
 
-class RestaurantAPI(Resource):
+class Restaurant(Resource):
     def get(self):
-        restaurant = db.Restaurant.query()
+        restaurant = db.Restaurant.query.all()
         return restaurant
 
     def post(self, name, address, price, ratings, open_close):
